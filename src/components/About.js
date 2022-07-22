a70310310e39a5140be5f6ccffff992200a0e978
Christopher Cerretani <cerretani@gmail.com>
2022-07-22 08:24:39 -0400

    fixed grammer

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
             <h2>About Me</h2>
             <p>
                 Hello, my name is Chris and I'm a community taught developer with a passion for learning, creating and problem solving.
-                I'm particularly interested open-source, decentraliztion, blockchain, smart contracts and the future of web3.
+                I'm particularly interested in open-source, decentraliztion, blockchain, smart contracts and the future of web3.
             </p>
         </section>
     );
