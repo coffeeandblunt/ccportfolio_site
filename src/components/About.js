b09652c8e3af7ee227e522508cba00342120cd66
coffeeandblunt <91853305+coffeeandblunt@users.noreply.github.com>
2022-08-20 23:14:27 -0400

    added scratch game to about
    embed banana chimp scratch game into body of about

--- src/components/About.js
+++ src/components/About.js
@@ -9,6 +9,9 @@ const About = () => {
                 Hello, my name is Chris and I'm a community taught developer with a passion for learning, creating and problem solving.
                 I'm particularly interested in open-source, decentraliztion, blockchain, smart contracts and the future of web3.
             </p>
+            <body>
+            <iframe src="https://scratch.mit.edu/projects/711632905/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>
+            </body>
         </section>
     );
 }
